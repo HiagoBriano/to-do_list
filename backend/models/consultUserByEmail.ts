a80ec5ed53e946b94da688c7325cd0783874e8ff
HiagoBriano <hiago.artist@hotmail.com>
2022-06-30 15:49:45 -0300

    test: user route test created
    check if it returns correctly either with all the information sent or with information missing

--- backend/models/consultUserByEmail.ts
+++ backend/models/consultUserByEmail.ts
@@ -1,10 +1,11 @@
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
+const teste = prisma.user;
 
 const consultUserByEmail = async (email: string) => {
   try {
-    const createdUser = await prisma.user.findUnique({
+    const createdUser = await teste.findUnique({
       where: { email },
       select: {
         id: true,
--- backend/tests/user.test.js
+++ backend/tests/user.test.js
@@ -8,50 +8,76 @@ const { expect } = require('chai');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-describe('Making a request on the login page', () => {
+describe('Creating a new user', () => {
   before(async () => {
-    sinon.stub(prisma.user, 'findUnique').resolves([
+    sinon.stub(prisma.user, 'create').resolves([
       {
-        id: 8,
+        id: 9,
         name: 'Brett Wiltshire',
-        email: 'brett@email.com',
-        password:
-          '$2a$04$6viIOEBjczjw7gbTT3pPKe0yPYWzbs7.LyCbzVVnbT5VVP8q26AvW',
+        email: 'brett2@email.com',
+        createIn: '2022-06-30T17:37:03.677Z',
       },
     ]);
   });
 
-  // afterEach(() => {
+  // after(() => {
   //   prisma.user.findUnique.restore();
   // });
 
-  it("checks if it is returning the user's token, name and e-mail, and if it doesn't return the review", async () => {
+  it('Submitting the correct information', async () => {
     const chaiHttpResponse = await chai
       .request('http://localhost:3001')
-      .post('/login')
+      .post('/user')
       .send({
-        email: 'brett@email.com',
+        name: 'Brett Wiltshire',
+        email: 'brett2@email.com',
         password: '123456',
       });
 
-    expect(chaiHttpResponse.status).to.be.eq(200);
+    expect(chaiHttpResponse.status).to.be.eq(201);
+    expect(chaiHttpResponse.body).to.have.property('id');
     expect(chaiHttpResponse.body).to.have.property('name');
     expect(chaiHttpResponse.body).to.have.property('email');
+    expect(chaiHttpResponse.body).to.have.property('createIn');
     expect(chaiHttpResponse.body).to.have.property('token');
     expect(chaiHttpResponse.body).to.not.have.property('password');
   });
 
-  it('without sending the email', async () => {
+  it('without the information sending the name', async () => {
     const chaiHttpResponse = await chai
       .request('http://localhost:3001')
-      .post('/login')
+      .post('/user')
       .send({
+        email: 'brett2@email.com',
         password: '123456',
       });
 
     expect(chaiHttpResponse.status).to.be.eq(400);
+    expect(chaiHttpResponse.body).to.not.have.property('id');
     expect(chaiHttpResponse.body).to.not.have.property('name');
     expect(chaiHttpResponse.body).to.not.have.property('email');
+    expect(chaiHttpResponse.body).to.not.have.property('createIn');
+    expect(chaiHttpResponse.body).to.not.have.property('token');
+    expect(chaiHttpResponse.body).to.not.have.property('password');
+    expect(chaiHttpResponse.body.message).to.be.eq(
+      'Sending the name is mandatory'
+    );
+  });
+
+  it('without the information sending the email', async () => {
+    const chaiHttpResponse = await chai
+      .request('http://localhost:3001')
+      .post('/user')
+      .send({
+        name: 'Brett Wiltshire',
+        password: '123456',
+      });
+
+    expect(chaiHttpResponse.status).to.be.eq(400);
+    expect(chaiHttpResponse.body).to.not.have.property('id');
+    expect(chaiHttpResponse.body).to.not.have.property('name');
+    expect(chaiHttpResponse.body).to.not.have.property('email');
+    expect(chaiHttpResponse.body).to.not.have.property('createIn');
     expect(chaiHttpResponse.body).to.not.have.property('token');
     expect(chaiHttpResponse.body).to.not.have.property('password');
     expect(chaiHttpResponse.body.message).to.be.eq(
@@ -59,71 +85,24 @@ describe('Making a request on the login page', () => {
     );
   });
 
-  it('without sending the password', async () => {
+  it('without the information sending the password', async () => {
     const chaiHttpResponse = await chai
       .request('http://localhost:3001')
-      .post('/login')
+      .post('/user')
       .send({
-        email: 'brett@email.com',
+        name: 'Brett Wiltshire',
+        email: 'brett2@email.com',
       });
 
     expect(chaiHttpResponse.status).to.be.eq(400);
+    expect(chaiHttpResponse.body).to.not.have.property('id');
     expect(chaiHttpResponse.body).to.not.have.property('name');
     expect(chaiHttpResponse.body).to.not.have.property('email');
+    expect(chaiHttpResponse.body).to.not.have.property('createIn');
     expect(chaiHttpResponse.body).to.not.have.property('token');
     expect(chaiHttpResponse.body).to.not.have.property('password');
     expect(chaiHttpResponse.body.message).to.be.eq(
       'Sending the password is mandatory'
     );
-  });
-
-  it('without sending email and password', async () => {
-    const chaiHttpResponse = await chai
-      .request('http://localhost:3001')
-      .post('/login')
-      .send({});
-
-    expect(chaiHttpResponse.status).to.be.eq(400);
-    expect(chaiHttpResponse.body).to.not.have.property('name');
-    expect(chaiHttpResponse.body).to.not.have.property('email');
-    expect(chaiHttpResponse.body).to.not.have.property('token');
-    expect(chaiHttpResponse.body).to.not.have.property('password');
-    expect(chaiHttpResponse.body.message).to.be.eq(
-      'Sending the email is mandatory'
-    );
-  });
-
-  it('invalid password', async () => {
-    const chaiHttpResponse = await chai
-      .request('http://localhost:3001')
-      .post('/login')
-      .send({
-        email: 'brett@email.com',
-        password: '1234567',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(400);
-    expect(chaiHttpResponse.body).to.not.have.property('name');
-    expect(chaiHttpResponse.body).to.not.have.property('email');
-    expect(chaiHttpResponse.body).to.not.have.property('token');
-    expect(chaiHttpResponse.body).to.not.have.property('password');
-    expect(chaiHttpResponse.body.message).to.be.eq('invalid password');
-  });
-
-  it('invalid email', async () => {
-    const chaiHttpResponse = await chai
-      .request('http://localhost:3001')
-      .post('/login')
-      .send({
-        email: 'brett@xablau.com',
-        password: '123456',
-      });
-
-    expect(chaiHttpResponse.status).to.be.eq(400);
-    expect(chaiHttpResponse.body).to.not.have.property('name');
-    expect(chaiHttpResponse.body).to.not.have.property('email');
-    expect(chaiHttpResponse.body).to.not.have.property('token');
-    expect(chaiHttpResponse.body).to.not.have.property('password');
-    expect(chaiHttpResponse.body.message).to.be.eq('Unregistered E-mail');
   });
 });
