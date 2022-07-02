8ffcce8beb72e4c9cb03cb10e15326eaa79328fe
HiagoBriano <hiago.artist@hotmail.com>
2022-07-02 11:49:19 -0300

    feat: registration page logic created
    send data in new user
    returns a warning if the user is already registered

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,84 +1,139 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
 import Layout from '../../components/Layout';
+import Loading from '../../components/Loading';
 import logotipo from '../../images/logotipo.png';
+import { CreateAccount } from '../../services/CreateAccount';
 import './register.css';
 
 function Register() {
-  const [nome, setNome] = useState('');
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [validatePassword, setValidatePassword] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [redirect, setRedirect] = useState(false);
+  const [status, setStatus] = useState('');
+  const [activeButton, setActiveButton] = useState(false);
+  const [toLogin, setToLogin] = useState(false);
+
+  useEffect(() => {
+    const regexEmail = /\S+@\S+\.\S+/;
+    if (
+      password.length > 5 &&
+      regexEmail.test(email) &&
+      password === validatePassword
+    ) {
+      setActiveButton(true);
+    } else {
+      setActiveButton(false);
+    }
+  }, [email, password, validatePassword]);
+
+  const validData = async (event: React.MouseEvent<HTMLElement>) => {
+    event.preventDefault();
+    setLoading(true);
+    setStatus('Estamos criando sua conta 😃');
+    const response = await CreateAccount(name, email, password);
+    console.log(response);
+    
+    if (response && response.message) {
+      setLoading(false);
+      alert('Ops, parece que você já tem cadastro com a gente');
+      setToLogin(true)
+      return;
+    }
+
+    setStatus('Só mais um pouco...');
+    localStorage.setItem('token', JSON.stringify(response.token));
+
+    setLoading(false);
+    setRedirect(true);
+  };
 
   return (
-    <Layout>
-      <form className="login-form">
-        <span className="login-form-title">Criar conta</span>
+    <>
+      {redirect && <Navigate to="/task" />}
+      {toLogin && <Navigate to="/" />}
+      {loading ? (
+        <Loading status={status} />
+      ) : (
+        <Layout>
+          <form className="register-form">
+            <span className="register-form-title">Criar conta</span>
 
-        <span className="login-form-title login-logotipo">
-          <img src={logotipo} alt="imagem da logotipo" />
-        </span>
+            <span className="register-form-title register-logotipo">
+              <img src={logotipo} alt="imagem da logotipo" />
+            </span>
 
-        <div className="wrap-input">
-          <input
-            className={nome.length > 0 ? 'input has-value' : 'input'}
-            type="text"
-            minLength={3}
-            value={nome}
-            onChange={({ target }) => setNome(target.value)}
-          />
-          <span className="focus-input" data-placeholder="Nome" />
-        </div>
+            <div className="wrap-input">
+              <input
+                className={name.length > 0 ? 'input has-value' : 'input'}
+                type="text"
+                minLength={3}
+                value={name}
+                onChange={({ target }) => setName(target.value)}
+              />
+              <span className="focus-input" data-placeholder="Nome" />
+            </div>
 
-        <div className="wrap-input">
-          <input
-            className={email.length > 0 ? 'input has-value' : 'input'}
-            type="email"
-            value={email}
-            onChange={({ target }) => setEmail(target.value)}
-          />
-          <span className="focus-input" data-placeholder="E-mail" />
-        </div>
+            <div className="wrap-input">
+              <input
+                className={email.length > 0 ? 'input has-value' : 'input'}
+                type="email"
+                value={email}
+                onChange={({ target }) => setEmail(target.value)}
+              />
+              <span className="focus-input" data-placeholder="E-mail" />
+            </div>
 
-        <div className="wrap-input">
-          <input
-            className={password.length > 0 ? 'input has-value' : 'input'}
-            type="password"
-            minLength={6}
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
-          <span className="focus-input" data-placeholder="Senha" />
-        </div>
+            <div className="wrap-input">
+              <input
+                className={password.length > 0 ? 'input has-value' : 'input'}
+                type="password"
+                minLength={6}
+                value={password}
+                onChange={({ target }) => setPassword(target.value)}
+              />
+              <span className="focus-input" data-placeholder="Senha" />
+            </div>
 
-        <div className="wrap-input">
-          <input
-            className={
-              validatePassword.length > 0 ? 'input has-value' : 'input'
-            }
-            type="password"
-            minLength={6}
-            value={validatePassword}
-            onChange={({ target }) => setValidatePassword(target.value)}
-          />
-          <span
-            className="focus-input"
-            data-placeholder="Confirme a sua senha"
-          />
-        </div>
+            <div className="wrap-input">
+              <input
+                className={
+                  validatePassword.length > 0 ? 'input has-value' : 'input'
+                }
+                type="password"
+                minLength={6}
+                value={validatePassword}
+                onChange={({ target }) => setValidatePassword(target.value)}
+              />
+              <span
+                className="focus-input"
+                data-placeholder="Confirme a sua senha"
+              />
+            </div>
 
-        <div className="container-login-form-btn">
-          <button className="login-form-btn">Registrar</button>
-        </div>
+            <div className="container-register-form-btn">
+              <button
+                className={`register-form-btn ${!activeButton && 'btn-off'}`}
+                onClick={(event) => validData(event)}
+                disabled={!activeButton}
+              >
+                Registrar
+              </button>
+            </div>
 
-        <div className="create-account-text-center">
-          <span className="create-account-text">Já possui conta?</span>
-          <Link to="/" className="create-account-link">
-            Fazer login.
-          </Link>
-        </div>
-      </form>
-    </Layout>
+            <div className="create-account-text-center">
+              <span className="create-account-text">Já possui conta?</span>
+              <Link to="/" className="create-account-link">
+                Fazer login.
+              </Link>
+            </div>
+          </form>
+        </Layout>
+      )}
+    </>
   );
 }
 
---
+++ frontend/src/services/CreateAccount.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios';
+import { API_URL } from './API_URL';
+
+export const CreateAccount = async (name: string, email: string, password: string) => {
+  const response = await axios
+    .post(`${API_URL}user`, {
+      name,
+      email,
+      password,
+    })
+    .then((response) => response.data)
+    .catch((error) => error);
+
+  return response;
+};
