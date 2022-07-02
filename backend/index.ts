2e11959d7b8d9e9872d22eae57dfd80520057f6b
HiagoBriano <hiago.artist@hotmail.com>
2022-07-02 15:04:17 -0300

    is pulling tasks from api

--- backend/index.ts
+++ backend/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import serverError from './middlewares/serverError';
 import routerLogin from './routers/routerLogin';
@@ -6,6 +7,7 @@ import routerUser from './routers/routerUser';
 require('dotenv/config');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001;
@@ -14,7 +16,7 @@ app.use('/user', routerUser);
 app.use('/login', routerLogin);
 app.use('/task', routerTask);
 app.get('/', (_req, res) => res.json({ message: 'active server' }));
-app.use(serverError)
+app.use(serverError);
 
 app.listen(+PORT, () => {
   console.log(`Online at the Port ${PORT}`);
