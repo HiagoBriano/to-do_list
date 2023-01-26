70e4b410758339f021d38147c74a4435638a4381
Hiago Briano <hiago.artist@hotmail.com>
2023-01-26 17:13:43 -0300

    variavel de ambiente para adicionar link de deploy adicionado

--- frontend/src/services/API_URL.tsx
+++ frontend/src/services/API_URL.tsx
@@ -1,4 +1,4 @@
 const host: string = process.env.REACT_APP_BACKEND_HOSTNAME || 'localhost';
 const port: string = process.env.REACT_APP_BACKEND_PORT || '3001';
 
-export const API_URL: string = `http://${host}:${port}/`;
+export const API_URL: string = process.env.REACT_APP_DEPLOY || `http://${host}:${port}/`;
