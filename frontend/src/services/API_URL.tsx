03ee6f5d28b97c5421f9cdb49fff6edebde44efc
Hiago Briano <hiago.artist@hotmail.com>
2023-01-26 21:26:25 -0300

    branch para deploy

--- frontend/src/services/API_URL.tsx
+++ frontend/src/services/API_URL.tsx
@@ -1,4 +1,1 @@
-const host: string = process.env.REACT_APP_BACKEND_HOSTNAME || 'localhost';
-const port: string = process.env.REACT_APP_BACKEND_PORT || '3001';
-
-export const API_URL: string = process.env.REACT_APP_DEPLOY || `http://${host}:${port}/`;
+export const API_URL: string = "https://to-do-list-backend-mazb.onrender.com/";
